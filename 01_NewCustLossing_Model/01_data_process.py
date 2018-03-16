40ecd2fe13297b0ec8dff63691f06546673428c6
luzhonghe <luzhonghe999@gmail.com>
2018-03-16 17:14:27 +0800

    提交

--- 01_NewCustLossing_Model/01_data_process.py
+++ 01_NewCustLossing_Model/01_data_process.py
@@ -4,7 +4,8 @@ import origin_data_preprocessing as odp
 
 if __name__=="__main__":
     work_file=r'F:/WorkProjects/Data/WorkDemo(MachineLearning)/01_NewCustLossing_Data/'
-    b=odr.origin_data_read(work_file + 'ModelData/',file_type='.csv',encoding_type='GBK')
+    b=odr.origin_data_read(work_file + 'ModelData/',file_type='.csv',encoding_type='GBK') # 读取文件
     origin_data=b.read_csv()
     odp.origin_data_preprocessing(origin_data).cal_index(work_file + 'ModelFile/origin_data_info.csv') # 输出异常检验
     origin_data = odp.origin_data_preprocessing(origin_data).fillna(-1) # 空值填充
+    
---
+++ 01_NewCustLossing_Model/construction_featrues.py
@@ -0,0 +1,24 @@
+class construction_featrues:
+    '''
+    '''
+    def __init__(self,data):
+        self.data = data
+    def cal_featrues(self):
+        df = self.data
+        df = df[(df['TARGET'] == 1) | (df['TARGET'] == 0)]
+        # (3)processing
+        # (3.1) date
+        df['open_datetime'] = pd.to_datetime(df['OPEN_DATE'], format='%Y%m%d')
+        df['valid_datetime'] = pd.to_datetime(df['FIRST_VALID_DATE'], format='%Y%m%d')
+        df['open_day'] = [i.day for i in df['open_datetime']]
+        df['valid_day'] = [i.day for i in df['valid_datetime']]
+        df['o_v_days'] = np.int64((df['valid_date'] - df['open_date']))/np.int64(86400e9)
+
+        # (3.2) prom
+        df_dummy_comp = pd.get_dummies(df['PROM_TYPE'], prefix='PROM', prefix_sep='_')
+        df = df.join(df_dummy_comp)
+
+        # (3.3) false
+        df['IS_1000'] = 0
+        df['ASSET_CHG'] = df['ASSET_NOW'] - df['ASSET_TOP']
+        df.loc[df['ASSET_TOP'] <= 1050, 'IS_1000'] = 1
